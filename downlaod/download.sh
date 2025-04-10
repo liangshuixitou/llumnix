c4c5ff65e16da22a7beeaf9dd2c577373871e5b5
l1hy <1151824226@qq.com>
2025-04-10 20:50:38 +0800

    2.5-3B

--- downlaod/download.sh
+++ downlaod/download.sh
@@ -1,6 +1,5 @@
-huggingface-cli download shibing624/sharegpt_gpt4  --repo-type dataset  --local-dir /home/ubuntu/data/dataset/sharegpt4 > download_dataset.log 2>&1
-
-huggingface-cli download  --repo-type model   --local-dir /home/ubuntu/data/model/Qwen-7B   Qwen/Qwen-7B
+huggingface-cli download shibing624/sharegpt_gpt4  --repo-type dataset  --local-dir /data/dataset/sharegpt4 
+huggingface-cli download  --repo-type model   --local-dir /data/model/Qwen2.5-3B   Qwen/Qwen2.5-3B
 
 
 
@@ -10,7 +9,7 @@ ray start --head --port=6379 --dashboard-host=0.0.0.0 --node-ip-address=36.103.1
 ray start --address=36.103.199.235:6379 --node-ip-address=43.143.179.203
 
 export RAY_DEDUP_LOGS=0 && python -m llumnix.entrypoints.vllm.serve --host 0.0.0.0 \
---port 37000  --model /home/ubuntu/data/model/Qwen-7B \
+--port 37000  --model /home/ubuntu/data/model/Qwen2.5-3B \
 --worker-use-ray --max-model-len 4096 \
 --dispatch-policy load --trust-remote-code \
 --dispatch-load-metric remaining_steps \
@@ -21,7 +20,7 @@ export RAY_DEDUP_LOGS=0 && python -m llumnix.entrypoints.vllm.serve --host 0.0.0
 --enable-port-increment --max-instances 3 
 
 export RAY_DEDUP_LOGS=0 && python -m llumnix.entrypoints.vllm.serve --host 0.0.0.0 \
---port 37000  --model /home/ubuntu/data/model/Qwen-7B \
+--port 37000  --model /home/ubuntu/data/model/Qwen2.5-3B \
 --worker-use-ray --max-model-len 4096 \
 --dispatch-policy rr --trust-remote-code \
 --request-migration-policy SR --migration-backend gloo \
@@ -32,7 +31,7 @@ export RAY_DEDUP_LOGS=0 && python -m llumnix.entrypoints.vllm.serve --host 0.0.0
 
 
 export RAY_DEDUP_LOGS=0 && python -m llumnix.entrypoints.vllm.serve --host 0.0.0.0 \
---port 37000  --model /home/ubuntu/data/model/Qwen-7B \
+--port 37000  --model /data/model/Qwen2.5-3B \
 --worker-use-ray --max-model-len 4096 \
 --dispatch-policy load --trust-remote-code \
 --dispatch-load-metric virtual_usage \
@@ -42,4 +41,7 @@ export RAY_DEDUP_LOGS=0 && python -m llumnix.entrypoints.vllm.serve --host 0.0.0
 --max-num-batched-tokens 16000 \
 --enable-port-increment --max-instances 3 
 
-2>&1 | tee vllm_load_virtual_usage.log
+
+
+ray start --head --port=6379 --dashboard-host=0.0.0.0 --node-ip-address=10.212.70.38
+ray start --address=10.212.70.38:6379
